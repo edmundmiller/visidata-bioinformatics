6b4e9d1a4a20132ed0a8f7d23da5fcb5347ebd38
Edmund Miller <git@edmundmiller.dev>
2024-12-29 21:24:09 -0600

    Revert "refactor: Simplify bed column getter function with lambda"
    This reverts commit 86a6cf7925771c9ef2c8a69569f405b5794d8f7e.

--- plugins/bed.py
+++ plugins/bed.py
@@ -63,13 +63,13 @@ class BedSheet(TsvSheet):
         self.rows = []
 
         def make_getter(idx, type_func=str):
-            def getter(row):
+            def getter(col, row):
                 try:
                     return type_func(row[idx]) if row and len(row) > idx else None
                 except (IndexError, ValueError, TypeError):
                     return None
 
-            return lambda col, row: getter(row)
+            return getter
 
         # Required BED fields
         self.addColumn(Column(name="chrom", type=str, getter_type=make_getter(0)))
